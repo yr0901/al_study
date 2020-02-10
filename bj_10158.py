779edd7b38ff9a4068b8598a85993301f5dcc9cd
yr0901 <yr0901@naver.com>
2020-02-10 18:00:36 +0900

    2020.02.10

--- bj_10158.py
+++ bj_10158.py
@@ -2,30 +2,28 @@
 w, h =map(int, input().split())
 p, q =map(int, input().split())
 t=int(input())
-def banbok(tempp, tempq, dx,dy):
-    if tempp==p and tempq==q and dx==1 and dy==1 and i!=1:
-        return True
+tempp=p
+tempq=q
 
-def ant(t):
-    dx=dy=1
-    global tempp, tempq
+def ant(tempp, tempq,t):
+    global p,q
+    dx=1
+    dy=1
     for i in range(1,t+1):
-        if banbok(tempp, tempq,dx,dy):
-            return t%i
         if tempp==0 or tempp==w:
             dx*=-1
         elif tempq==0 or tempq==h:
             dy*=-1
         tempp+=dx
         tempq+=dy
-    return [tempp,tempq]
-tempp=p
-tempq=q
-i=1
-anwlist=[]
-while True:
-    if type(ant(t))=="<class 'int'>":
-        anwlist=ant(ant(tempp, tempq))
-    else: anwlist+=ant(t)
 
-print(*anwlist)
+        if  tempp==p and tempq==q and dx==1 and dy==1: #반복찾기
+            i=t%i
+            break
+    return tempp, tempq, i
+
+resultp, resultq, i = ant(tempp, tempq,t)
+if i!=t:
+    resultp, resultq,i = ant(resultp, resultq,i)
+    
+print(resultp, resultq)
