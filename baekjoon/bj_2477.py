dfb2e8f8d1eb80de2d0643043148fc24335ddf8c
suhyunHwang <saeyu93@gmail.com>
2020-02-18 18:04:51 +0900

    2020.02.18

--- baekjoon/bj_2477.py
+++ baekjoon/bj_2477.py
@@ -1,13 +1,27 @@
 #참외밭
 km=int(input())
-tlist=[]
 
 #동:1 서:2 남:3 북:4
 x,y=0,0
-arr = [[0 for _ in range(500)] for _ in range(500)]
+#arr = [[0 for _ in range(500)] for _ in range(500)]
+tlist=[list(map(int, input().split())) for _ in range(6)]
+
+maxx=maxy=maxydir=maxxdir=0
+
 for i in range(6):
-    d,m=map(int, input().split())
-    if 
+    dir=tlist[i][0]
+    kilo=tlist[i][1]
+
+    if dir==4 or dir==3 and kilo>maxy:
+        maxy=kilo
+        maxydir=dir
+
+    elif dir==1 or dir==2 and kilo>maxx:
+        maxx=kilo
+        maxxdir=dir
+
+
+
 
 
         
--- baekjoon/bj_2559.py
+++ baekjoon/bj_2559.py
@@ -1,11 +1,16 @@
 N, K = map(int, input().split())
 tlist=list(map(int, input().split()))
 maxn=-12345
-
-for n in range(N-K):
-    summ=0
-    for k in range(K):
-        summ+=tlist[n+k]
+summ=0
+for i in range(0,K):
+    summ+=tlist[i]
+if summ>maxn:
+    maxn=summ
+n=0
+while n<=N-K-1:
+    summ=summ-tlist[n]+tlist[n+K]
     if summ>maxn:
         maxn=summ
+    n+=1
+
 print(maxn)
