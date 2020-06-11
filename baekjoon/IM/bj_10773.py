93fcfcdf517fe0f3736744090039e827445fc410
leeyeeun <yr0901@naver.com>
2020-06-11 12:51:09 +0900

    0611

--- baekjoon/IM/bj_10773.py
+++
@@ -1,9 +0,0 @@
-K=int(input())
-stack=[]
-for _ in range(K):
-    t=int(input())
-    if t!=0:
-        stack.append(t)
-    else:
-        stack.pop()
-print(sum(stack))
--- baekjoon/IM/bj_2605.py
+++
@@ -1,9 +0,0 @@
-#줄세우기
-stu=int(input())
-tlist=[]
-clist=list(map(int, input().split()))
-
-for i in range(stu):
-    change = clist[i]
-    tlist.insert(i-change,i+1)
-print(*tlist)
